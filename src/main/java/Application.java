55b021fc866f582622f82a37f221cab82253ce57
voilacf <vio.lafe.vf@gmail.com>
2023-02-21 23:21:38 +0100

    updated application and test management

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,17 @@
+import account.AccountType;
+import account.IAccount;
+import user.Administrator;
+
+import java.util.ArrayList;
+
 public class Application {
 
     public static void main(String[] args){
+        Administrator admin = new Administrator("Test");
+        admin.createAccount("Test", AccountType.ACTIVE,100.00);
+    }
+
+    public void init(){
 
     }
 }
--- src/test/java/TestManagement.java
+++ src/test/java/TestManagement.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestManagement {
+    private Application application;
+
+    @BeforeEach
+    public void setup() {
+        application = new Application();
+        application.init();
+    }
 }
