6dcc4f1c251108773a7aee0b6737db17c352a717
voilacf <vio.lafe.vf@gmail.com>
2023-03-04 02:32:06 +0100

    preparations for cucumber tests

---
+++ src/test/java/command/CommandStepdefs.java
@@ -0,0 +1,10 @@
+package command;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CommandStepdefs {
+}
---
+++ src/test/java/memento/MementoStepdefs.java
@@ -0,0 +1,10 @@
+package memento;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MementoStepdefs {
+}
